daff5ee722b02c659d1e65823b09334703bfaa84
Andres Mora <43217729+andresmoradevs@users.noreply.github.com>
2023-12-07 14:29:24 -0500

    struct-toolbar-ft-commit

---
+++ src/app/pages/eco/eco.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eco',
+  templateUrl: './eco.page.html',
+  styleUrls: ['./eco.page.scss'],
+})
+export class EcoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
